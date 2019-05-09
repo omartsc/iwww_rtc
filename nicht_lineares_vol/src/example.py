d3ededd379d8c8829200dba1648446942e1d2c7c
Omar Tapia-Santacruz <omar.tsc@gmail.com>
2019-05-09 15:15:34 +0200

    Model für sequenzielle Füllung von 2 Wasserspeichern

---
+++ nicht_lineares_vol/src/example.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+
+from rtctools.optimization.collocated_integrated_optimization_problem \
+    import CollocatedIntegratedOptimizationProblem
+from rtctools.optimization.csv_mixin import CSVMixin
+from rtctools.optimization.modelica_mixin import ModelicaMixin
+from rtctools.util import run_optimization_problem
+
+
+class Example(CSVMixin, ModelicaMixin, CollocatedIntegratedOptimizationProblem):
+    """
+    This class is the optimization problem for the Example. Within this class,
+    the objective, constraints and other options are defined.
+    """
+
+    # This is a method that returns an expression for the objective function.
+    # RTC-Tools always minimizes the objective.
+    def objective(self, ensemble_member):
+
+        # Minimize water pumped. The total water pumped is the integral of the
+        # water pumped from the starting time until the stoping time. In
+        # practice, self.integral() is a summation of all the discrete states.
+
+        return self.integral('Q_KBW_2', ensemble_member)
+
+    # A path constraint is a constraint where the values in the constraint are a
+    # Timeseries rather than a single number.
+    def path_constraints(self, ensemble_member):
+        # Call super to get default constraints
+        constraints = super().path_constraints(ensemble_member)
+
+        constraints.append(
+            (self.state('Q_KBW') + (1 - self.state('is_full')) * 18,
+                0.0, 18.0))
+
+        M = 2
+        constraints.append(
+            (self.state('H_full') - self.state('TSP.HQ.H')
+                - (1 - self.state('is_full')) * M,
+                -np.inf, 0.0))
+
+        constraints.append((self.state('H_full') - self.state('TSP.HQ.H')
+                            + self.state('is_full') * M, 0.0, np.inf))
+
+        constraints.append((self.state('TSP.HQ.H'), 0.2, 0.8))
+
+        constraints.append((self.state('UWB.HQ.H'), 0.1, 0.6))
+
+        return constraints
+
+    # Any solver options can be set here
+    def solver_options(self):
+        options = super().solver_options()
+        # Restrict solver output
+        solver = options['solver']
+        options[solver]['print_level'] = 1
+        return options
+
+
+# Run
+run_optimization_problem(Example)
